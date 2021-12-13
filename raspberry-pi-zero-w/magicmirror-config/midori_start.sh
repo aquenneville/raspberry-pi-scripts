168d86ccb34109377b0e43e0ea0b65996e5b2ea0
aquenneville <alainquenneville@gmail.com>
2021-12-13 16:28:40 +0000

    Update midori_start.sh

--- raspberry-pi-zero-w/magicmirror-config/midori_start.sh
+++ raspberry-pi-zero-w/magicmirror-config/midori_start.sh
@@ -4,8 +4,6 @@ xset -dpms # disable DPMS (Energy Star) features.
 xset s off # disable screen saver
 xset s noblank # don’t blank the video device
 matchbox-window-manager &
-#chromium-browser --incognito --kiosk http://localhost:8080/ #these are two single dashes before incognito and kiosk
-#midori -a http://localhost:8080 -i 120 -e Fullscreen
 midori -a http://localhost:8080 -e Fullscreen
 while :
 do  
