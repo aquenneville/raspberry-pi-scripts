39d1c46502fb2cb6c3c3e2d239e8a7d5acdd93c5
aquenneville <alainquenneville@gmail.com>
2021-12-13 16:38:25 +0000

    changes

---
+++ zero-w/home/pi/midori_start.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+#unclutter & # remove the mouse pointer
+xset -dpms # disable DPMS (Energy Star) features.
+xset s off # disable screen saver
+xset s noblank # don’t blank the video device
+matchbox-window-manager &
+midori -a http://localhost:8080 -e Fullscreen
+while :
+do  
+    sleep 120;
+    echo "Reload midori"
+    midori -e Reload
+done 
